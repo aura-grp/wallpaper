21c5cab9bffc54c5e1e2c95855ef6691bfbc6211
aura-grp <bgis2k24@gmail.com>
2025-08-24 20:31:15 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-
-
   return (
-    <div className="py-7 px-15 bg-[] text-white flex items-center justify-between">
-      <h1 className="font-bacley text-7xl ">Frame</h1>
-      <div className="flex gap-10 ">
-        <Link to="/">Home</Link>
-        <Link to="/cars">Cars</Link>
-        <Link to="/minimal">Minimal</Link>
-        <Link to="/black">Black</Link>
+    <nav className="bg-[#00060a] text-white px-6 py-5 flex flex-col md:flex-row md:items-center md:justify-between">
+      {/* Logo */}
+      <h1 className="font-bacley text-5xl mb-4 md:mb-0 md:text-6xl text-center md:text-left">
+        Frame
+      </h1>
+
+      {/* Nav Links */}
+      <div className="flex justify-center md:justify-end gap-6 md:gap-10 text-lg">
+        <Link to="/" className="hover:text-gray-300">Home</Link>
+        <Link to="/cars" className="hover:text-gray-300">Cars</Link>
+        <Link to="/minimal" className="hover:text-gray-300">Minimal</Link>
+        <Link to="/black" className="hover:text-gray-300">Black</Link>
       </div>
-    </div>
+    </nav>
   );
 };
 
